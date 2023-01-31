b9e886b9a003db04cc6e7bc40a7fd11985e2667f
mikevarriano <113132199+mikevarriano@users.noreply.github.com>
2023-01-31 14:00:46 -0500

    sds

--- viteproj/main.js
+++ viteproj/main.js
@@ -1,18 +1,20 @@
 import "./style.css";
 
-const input = prompt("Enter a GitHub username:");
-const displayDiv = document.getElementById("display-data");
+const input = prompt(
+  "Enter a Public GitHub username and the crystal ball will tell you all:"
+);
+const displayDiv = document.getElementById("mvarriano");
 
 fetch(`https://api.github.com/users/${input}`)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
     displayDiv.innerHTML = `
       <p>Name: ${data.name}</p>
       <p>Location: ${data.location}</p>
       <p>Followers: ${data.followers}</p>
     `;
   })
-  .catch(error => {
+  .catch((error) => {
     console.error("An error occurred: " + error);
   });
-
